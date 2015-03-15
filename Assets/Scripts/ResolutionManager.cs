59aeb65d22a8956ea2b27f9a12e54df15256343e
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-03-15 21:54:14 +0000

    Started resolution in options menu

---
+++ Assets/Scripts/ResolutionManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResolutionManager : MonoBehaviour {
+
+    public Toggle Fullscreen;
+    public InputField widthInput;
+    public InputField heightInput;
+
+    private int MonitorHeight;
+    private int MonitorWidth;
+
+    private int desiredHeight;
+    private int desiredWidth;
+
+    void Start()
+    {
+        MonitorHeight = Screen.currentResolution.height;
+        MonitorWidth = Screen.currentResolution.width;
+    }
+
+    void Update()
+    {
+            //TODO: WORKING CODE
+    }
+
+    void UpdateWidth(int width)
+    {
+        desiredWidth = width;
+    }
+
+    void UpdateHeight(int height)
+    {
+        desiredHeight = height;
+    }
+}
