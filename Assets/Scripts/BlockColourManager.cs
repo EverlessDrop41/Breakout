850a619a59759b810e4b0b5e2fd9678c788cc0a3
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-03-20 22:54:14 +0000

    Began Work on the Block Colour Change

---
+++ Assets/Scripts/BlockColourManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BlockColourManager : EditorWindow {
+
+    public static Sprite SuperHardBlock;
+    public static Sprite HardBlock;
+    public static Sprite MediumBlock;
+	public static Sprite WeakBlock;
+
+    [MenuItem("Window/Block Color Setter")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(BlockColourManager));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Set Sprites", EditorStyles.boldLabel);
+        EditorGUILayout.BeginHorizontal();
+        SuperHardBlock = EditorGUILayout.ObjectField(SuperHardBlock, typeof(Sprite), true);
+        EditorGUILayout.EndHorizontal();
+    }
+}
