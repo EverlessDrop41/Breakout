ef4bf4fe39c2511a25be5cc8c64bdcdeb46fe45e
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-03-21 08:15:43 +0000

    Blocks now change colour based on health

--- Assets/Scripts/BlockColourManager.cs
+++ Assets/Scripts/BlockColourManager.cs
@@ -1,30 +1,22 @@
 ﻿using UnityEngine;
-using UnityEditor;
 
-public class BlockColourManager : EditorWindow {
+public class BlockColourManager : MonoBehaviour {
 
-    public static Sprite SuperHardBlock;
-    public static Sprite HardBlock;
-    public static Sprite MediumBlock;
-	public static Sprite WeakBlock;
+    public Sprite SuperHardBlock;
+    public Sprite HardBlock;
+    public Sprite MediumBlock;
+	public Sprite WeakBlock;
 
-    [MenuItem("Window/Block Color Setter")]
-    public static void ShowWindow()
+    public static Sprite SuperHard;
+    public static Sprite Hard;
+    public static Sprite Medium;
+    public static Sprite Weak;
+
+    void Start()
     {
-        EditorWindow.GetWindow(typeof(BlockColourManager));
-    }
-
-    void OnGUI()
-    {
-        GUILayout.Label("Set Sprites", EditorStyles.boldLabel);
-
-        GUILayout.Label("Super Hard Block", EditorStyles.label);
-        SuperHardBlock =  EditorGUILayout.ObjectField(SuperHardBlock,typeof(Sprite), false) as Sprite;
-        GUILayout.Label("Hard Block", EditorStyles.label);
-        HardBlock = EditorGUILayout.ObjectField(HardBlock, typeof(Sprite), false) as Sprite;
-        GUILayout.Label("Medium Block", EditorStyles.label);
-        MediumBlock = EditorGUILayout.ObjectField(MediumBlock, typeof(Sprite), false) as Sprite;
-        GUILayout.Label("Weak Block", EditorStyles.label);
-        WeakBlock = EditorGUILayout.ObjectField(WeakBlock, typeof(Sprite), false) as Sprite;
+        SuperHard = SuperHardBlock;
+        Hard = HardBlock;
+        Medium = MediumBlock;
+        Weak = WeakBlock;
     }
 }
--- Assets/Scripts/BreakoutBlock.cs
+++ Assets/Scripts/BreakoutBlock.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class BreakoutBlock : MonoBehaviour {
 	///<summary>
 	/// 	The different types of beakout block available
@@ -14,22 +15,30 @@ public class BreakoutBlock : MonoBehaviour {
 	
 	public blockType Type;
 	private int maxHealth;
+    private SpriteRenderer SprRend;
 
 	private int currentHealth;
 
 	void Start () {
+
+        SprRend = GetComponent<SpriteRenderer>();
+
 		switch (Type) {
 			case blockType.weak:
 				maxHealth = 1;
+                SprRend.sprite = BlockColourManager.Weak;
 				break;
 			case blockType.medium:
 				maxHealth = 2;
+                SprRend.sprite = BlockColourManager.Medium;
 				break;
 			case blockType.hard:
 				maxHealth = 3;
+                SprRend.sprite = BlockColourManager.Hard;
 				break;
 			case blockType.superHard:
 				maxHealth = 4;
+                SprRend.sprite = BlockColourManager.SuperHard;
 				break;
 			default:
 				Debug.LogError("The type of block is not defined");
@@ -40,6 +49,19 @@ public class BreakoutBlock : MonoBehaviour {
 
 	void TakeDamage(){
 		currentHealth--;
+
+        switch (currentHealth)
+        {
+            case 3:
+                SprRend.sprite = BlockColourManager.Hard;
+                break;
+            case 2:
+                SprRend.sprite = BlockColourManager.Medium;
+                break;
+            case 1:
+                SprRend.sprite = BlockColourManager.Weak;
+                break;
+        }
 	}
 
 	void Update () {
